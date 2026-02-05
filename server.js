8592eed0c82e2cb82122a7521c06928621db708d
stefanobellomo <stefanobellomo1998@gmail.com>
2026-02-05 11:26:32 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+function somma(primo, secondo) {
+    return primo + secondo
+}
+
+console.log(somma(1, 2));
+
+
+somma(1, 2)
+
+let sum = function (primo, secondo) {
+    return primo + secondo
+}
+
+const tot = sum(2, 2)
+console.log(tot);
+
+
+
+
