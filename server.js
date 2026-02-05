e05f698ab6dd0c7b0d66bdca504ef5aee0ad730a
stefanobellomo <stefanobellomo1998@gmail.com>
2026-02-05 12:10:07 +0100

    Complete some snack

--- server.js
+++ server.js
@@ -1,18 +1,53 @@
+// SNACK 1
+
+// forma base
 function somma(primo, secondo) {
     return primo + secondo
 }
 
 console.log(somma(1, 2));
 
-
-somma(1, 2)
-
+// funzione anonima assegnata a variabile
 let sum = function (primo, secondo) {
     return primo + secondo
 }
 
 const tot = sum(2, 2)
 console.log(tot);
+
+// arrow function assegnata a variabile
+let somm = (primo, secondo) => primo + secondo
+
+const totale = somm(3, 3)
+console.log(totale);
+
+// SNACK 2
+
+const quadrato = (numb) => numb * numb
+
+console.log(quadrato(4));
+
+// SNACK 3
+
+let sottrazione = (a, b) => a - b
+let divisione = (a, b) => a / b
+
+const eseguiOperazione = (a, b, operazione) => operazione(a, b)
+
+console.log(eseguiOperazione(2, 2, sottrazione));
+console.log(eseguiOperazione(9, 3, divisione));
+
+// SNACK 4
+
+function creaTimer() {
+    console.log(('tempo scaduto'));
+}
+
+setTimeout(creaTimer, 4000)
+
+// SNACK 5
+
+
 
 
 
